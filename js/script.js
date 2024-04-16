00c89008665fbe5750a1d00d1fc5a5da0b59ee80
ChurrosCtmre <edugonzalezsilva72@gmail.com>
2024-04-16 15:32:42 -0400

    arreglo x

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
+    event.preventDefault();
     
-    // Aquí puedes agregar código para enviar el formulario, si lo deseas
+
     
     // Mostrar el mensaje de "Mensaje enviado"
     document.getElementById("mensajeEnviado").style.display = "block";
     
-    // Puedes agregar más acciones aquí, como limpiar los campos del formulario, etc.
+
 });
